43b414e5009a5b93e9dbf61242f89b67b07e7d83
omagaDL <57490476+omagaDL@users.noreply.github.com>
2023-02-07 09:10:55 +0300

    Update B.cpp

--- week01/B.cpp
+++ week01/B.cpp
@@ -7,7 +7,7 @@ int main(){
     cin >> a >> b >> c;
     int D = b*b - 4*a*c;
     if (D > 0){
-        cout << (0 - sqrt(D) - b)/(2*a) << ' ' << (sqrt(D) - b)/(2*a) << endl;
+        cout << (0 - sqrt(D) - b)/(2*a) << " " << (sqrt(D) - b)/(2*a) << endl;
     }else if(D == 0){
         cout << (-b)/(2*a) << endl;
     }else{
