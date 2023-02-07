5a13b5a70cbe72e7317968334de002add97fdd53
omagaDL <57490476+omagaDL@users.noreply.github.com>
2023-02-07 09:09:23 +0300

    Add files via upload

--- week01/B.cpp
+++ week01/B.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main(){
     int a=0, b=0, c=0;
     cin >> a >> b >> c;
-    if ((b*b-4*a*c) > 0){
-        cout << (-sqrt(b*b-4*a*c)-b)/(2*a) << ' ' << (sqrt(b*b-4*a*c)-b)/(2*a) << endl;
-    }else if((b*b-4*a*c) == 0){
-        cout << (sqrt(b*b-4*a*c)-b)/(2*a) << endl;
+    int D = b*b - 4*a*c;
+    if (D > 0){
+        cout << (0 - sqrt(D) - b)/(2*a) << ' ' << (sqrt(D) - b)/(2*a) << endl;
+    }else if(D == 0){
+        cout << (-b)/(2*a) << endl;
     }else{
         cout << '\n' << endl;
     }
