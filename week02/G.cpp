038a186d30e303c87614fe29e194291c2a094a17
omagaDL <dmitrij.lejnew@yandex.ru>
2023-02-16 12:20:08 +0300

    week02

---
+++ week02/G.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int reverse_number(long long number){
+    long long n = 0;
+    while(number){
+        n = 10*n + number%10;
+        number /= 10;
+    }
+    return n;
+}
+
+int main(){
+    
+    long long n = 0;
+    cin >> n;
+    if (reverse_number(n) == n){
+        cout << "YES" << endl;
+    } else{
+        cout << "NO" << endl;
+    }
+}
